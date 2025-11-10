d54ed7997dd6b6686d87dc8a7f4312593fa9a751
Einschild <84232092+Einschild@users.noreply.github.com>
2025-11-11 00:52:25 +0800

    Time: 299 ms (86.28%) | Memory: 17.9 MB (48.26%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -1,7 +1,8 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
-        res = {}
+        minDiff = float('inf')
+        res = float('inf')
 
         for i in range(len(nums) - 2):
             x = nums[i]
@@ -11,9 +12,12 @@ class Solution:
             while j < k:
                 total = x + nums[j] + nums[k]
                 diff = abs(total - target)
-                res[diff] = total
+
+                if diff < minDiff:
+                    minDiff = diff
+                    res = total
                 if total < target:
                     j += 1
                 else:
                     k -= 1
-        return res[min(res.keys())]
+        return res
