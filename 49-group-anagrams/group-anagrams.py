6a4cc2c308b87f4469de01aecbd1d8cb7f9312d7
Einschild <84232092+Einschild@users.noreply.github.com>
2025-11-12 01:32:25 +0800

    Time: 15 ms (50.03%) | Memory: 22.6 MB (24.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,13 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = {}
+        res = defaultdict(list)
 
         for anagram in strs:
-            freq = [0 for _ in range(26)]
+            freq = [0] * 26
 
             for char in anagram:
-                freq[ord(char) - 97] += 1
+                freq[ord(char) - ord('a')] += 1
             
-            res.setdefault(tuple(freq), []).append(anagram)
+            res[tuple(freq)].append(anagram)
         
         return list(res.values())
