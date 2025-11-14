1f45bdcafe98c801cc27a8f8b4c38df183362e57
Einschild <84232092+Einschild@users.noreply.github.com>
2025-11-14 23:44:38 +0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (80.46%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -5,17 +5,15 @@ class Solution:
 
         low = 1
         high = x // 2
-        res = 0
 
         while low <= high:
             mid = (low + high) // 2
             midSq = mid * mid
             if midSq < x:
-                res = mid
                 low = mid + 1
             elif midSq > x:
                 high = mid - 1
             else:
                 return mid
         
-        return res
+        return high
