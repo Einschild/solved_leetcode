9a12d170c48384d85ba5f904f246023f7af835cb
Einschild <84232092+Einschild@users.noreply.github.com>
2025-11-30 00:50:39 +0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (89.88%) - LeetSync

--- 504-base-7/base-7.py
+++ 504-base-7/base-7.py
@@ -6,6 +6,6 @@ class Solution:
         res = ''
         absNum = abs(num)
         while absNum > 0:
-            res = f'{absNum % 7}' + res
+            res = str(absNum % 7) + res
             absNum //= 7
-        return res if num > 0 else f'{int(res) * -1}'
+        return res if num > 0 else str(int(res) * -1)
