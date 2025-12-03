8e382b5d81ac570d6c5749e934f85322241021c2
Einschild <84232092+Einschild@users.noreply.github.com>
2025-12-04 01:40:31 +0800

    Time: 27 ms (30.88%) | Memory: 19.5 MB (55.31%) - LeetSync

--- 575-distribute-candies/distribute-candies.py
+++ 575-distribute-candies/distribute-candies.py
@@ -1,9 +1,5 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        seen = set(candyType)
+        types = set(candyType)
         
-        res = len(seen)
-        if res > len(candyType) // 2:
-            res = len(candyType) // 2
-        
-        return res
+        return min(len(types), len(candyType) // 2)
