3f148dba36fc2ccad33bde3d66523470c61f08cd
Einschild <84232092+Einschild@users.noreply.github.com>
2025-12-12 23:44:57 +0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (42.44%) - LeetSync

---
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        ptr = 0
+        while ptr < len(bits) - 1:
+            if bits[ptr] == 1:
+                ptr += 2
+            else:
+                ptr += 1
+        
+        return (ptr - len(bits)) % 2 == 1
